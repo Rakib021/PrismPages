8586e43a36f0a0f895417caa6bbba21299982aea
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-10 12:13:46 +0600

    static_files

--- PrismPages/settings.py
+++ PrismPages/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5b!$(32o4o#hiazgl97pz_sg$#v3dim%!)2932dzg*bo^h$j3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 MEDIA_URL ='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- PrismPages/urls.py
+++ PrismPages/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('',include('app_users.urls'))
 
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns() # new
